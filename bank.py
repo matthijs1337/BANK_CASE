3954760eb0b51a17abb3ac53c7f0a437ee494792
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-16 13:40:39 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
+bank = pd.read_csv("bank.csv", sep=';')
 
-data = {'Feature1': [1, 2, 3, 4, 5],'Feature2': [2, 3, 4, 5, 6],'Target': ['Nee', 'Nee', 'Ja', 'Nee', 'Ja']}
-df = pd.DataFrame(data)
+
 
 # Train een logistisch regressiemodel met de dataset
-X = df[['Feature1', 'Feature2']]
-y = df['Target']
+X = bank[['job', 'loan']]
+y = bank['y']
 model = LogisticRegression()
 model.fit(X, y)
 
@@ -16,11 +16,11 @@ model.fit(X, y)
 st.title('Ja/Nee Voorspellingsdashboard')
 
 # Dropdown-menu's voor variabelen
-feature1 = st.selectbox('Selecteer Feature1:', df['Feature1'].unique())
-feature2 = st.selectbox('Selecteer Feature2:', df['Feature2'].unique())
+job = st.selectbox('Selecteer job:', bank['job'].unique())
+loan = st.selectbox('Selecteer loan:', bank['loan'].unique())
 
 # Maak een voorspelling met het model op basis van de geselecteerde waarden
-prediction = model.predict([[feature1, feature2]])
+prediction = model.predict([[job, loan]])
 
 # Toon het voorspelde resultaat
 st.write(f"Voorspelling: {prediction[0]}")
