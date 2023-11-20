15b6900f694e00de8f9d8b2b42350241c253a5c4
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-20 17:05:13 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -6,19 +6,24 @@ df = pd.read_csv("bank.csv", sep=';')
 df = df.drop_duplicates()
 
 #categoriseren van data
+#leeftijd
 df.loc[df['age'] < 30, 'AgeGroup'] = '17-29'
 df.loc[(df['age'] >= 30) & (df['age'] < 50), 'AgeGroup'] = '30-49'
 df.loc[(df['age'] >= 50) & (df['age'] < 65), 'AgeGroup'] = '50-64'
 df.loc[df['age'] >= 65, 'AgeGroup'] = '65+'
 
+#opleiding
+
+
 # Labelcodering toepassen
 label_encoder = LabelEncoder()
 df['job_encoded'] = label_encoder.fit_transform(df['job'])
 df['age_encoded'] = label_encoder.fit_transform(df['AgeGroup'])
+df['education_encoded'] = label_encoder.fit_transform(df['education'])
 
 
 # Train een logistisch regressiemodel met de dataset
-X = df[['job_encoded', 'pdays', 'age_encoded']]
+X = df[['job_encoded', 'pdays', 'age_encoded', 'education_encoded']]
 y = df['y']
 model = LogisticRegression()
 model.fit(X, y)
@@ -27,8 +32,9 @@ st.title('Ja/Nee Voorspellingsdashboard')
 # Dropdown-menu's voor variabelen
 job_encoded = st.selectbox('Selecteer baan:', df['job_encoded'].unique())
 age_encoded = st.selectbox('Selecteer leeftijdsgroep:', df['age_encoded'].unique())
+education_encoded = st.selectbox('Selecteer opleidingsniveau:', df['education_encoded'].unique())
 pdays = st.selectbox('Selecteer pdays:', df['pdays'].unique())
 # Maak een voorspelling met het model op basis van de geselecteerde waarden
-prediction = model.predict([[job_encoded, age_encoded, pdays]])
+prediction = model.predict([[job_encoded, age_encoded, education_encoded, pdays]])
 # Toon het voorspelde resultaat
 st.write(f"Voorspelling: {prediction[0]}")
