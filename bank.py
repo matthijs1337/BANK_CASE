f2b62a6974c2e2759b86f804f310a6b6eadba700
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-23 13:57:54 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -32,7 +32,11 @@ df['duration_encoded'] = label_encoder.fit_transform(df['DurationGroup'])
 
 
 # Train een logistisch regressiemodel met de dataset
-X = df[['job_encoded', 'pdays', 'age_encoded', 'education_encoded']]
+X = df[['age_encoded', 'job_encoded',
+        'marital_encoded', 'contact_encoded'
+        'education_encoded', 'contact_encoded'
+        'month_encoded', 'duration_encoded'
+       ]]
 y = df['y']
 model = LogisticRegression()
 model.fit(X, y)
