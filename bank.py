15ef660296b1d92274d38e71050510cddba6b832
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-27 16:50:08 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -74,8 +74,28 @@ def tab_one():
 
 def plot_bar_charts():
     #figuur 1 leeftijdsgroep
-    fig1 = px.pie(df, names='AgeGroup', values='y', title='Verdeling per leeftijdscategorie')
-    st.plotly_chart(fig1)
+    #fig1 = px.pie(df, names='AgeGroup', values='y', title='Verdeling per leeftijdscategorie')
+    #st.plotly_chart(fig1)
+    # Create subplots: use 'domain' type for Pie subplot
+    fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
+
+    # Create pie chart for 'yes'
+    fig.add_trace(go.Pie(labels=df['AgeGroup'], values=df['y'], name="Yes"), 1, 1)
+
+    # Create pie chart for 'no'
+    fig.add_trace(go.Pie(labels=df['AgeGroup'], values=df['y'], name="No"), 1, 2)
+
+    # Use `hole` to create a donut-like pie chart
+    fig.update_traces(hole=0.4, hoverinfo="label+percent+name")
+
+    fig.update_layout(
+    title_text="Distribution of 'yes' and 'no' based on AgeGroup",
+    # Add annotations in the center of the donut pies.
+    annotations=[
+        dict(text='Yes', x=0.18, y=0.5, font_size=20, showarrow=False),
+        dict(text='No', x=0.82, y=0.5, font_size=20, showarrow=False)])
+    
+    st.plotly_chart(fig)
 
     
     #figuur 2 lengte van de call
