ef1b2fb0a46721b1dbe92519a7285becbe1ad768
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-28 23:12:44 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -7,7 +7,10 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 #Data inladen
 df = pd.read_csv("bank.csv", sep=';')
 
@@ -74,6 +77,54 @@ def tab_one():
     # Toon het voorspelde resultaat
     st.write(f"Voorspelling: {prediction[0]}")
 
+    #Converting dependent variable categorical to dummy
+    y = pd.get_dummies(bank['y'], columns = ['y'], prefix = ['y'], drop_first = True)
+      
+    bank_client = bank.iloc[: , 0:7]
+    # Label encoder order is alphabetical
+    from sklearn.preprocessing import LabelEncoder
+    labelencoder_X = LabelEncoder()
+    bank_client['job']      = labelencoder_X.fit_transform(bank_client['job']) 
+    bank_client['marital']  = labelencoder_X.fit_transform(bank_client['marital']) 
+    bank_client['education']= labelencoder_X.fit_transform(bank_client['education']) 
+    bank_client['default']  = labelencoder_X.fit_transform(bank_client['default']) 
+    bank_client['housing']  = labelencoder_X.fit_transform(bank_client['housing']) 
+    bank_client['loan']     = labelencoder_X.fit_transform(bank_client['loan']) 
+    bank_related = bank.iloc[: , 7:11]
+    # Label encoder order is alphabetical
+    from sklearn.preprocessing import LabelEncoder
+    labelencoder_X = LabelEncoder()
+    bank_related['contact']     = labelencoder_X.fit_transform(bank_related['contact']) 
+    bank_related['month']       = labelencoder_X.fit_transform(bank_related['month']) 
+    bank_related['day_of_week'] = labelencoder_X.fit_transform(bank_related['day_of_week']) 
+    def duration(data):
+    
+        data.loc[data['duration'] <= 102, 'duration'] = 1
+        data.loc[(data['duration'] > 102) & (data['duration'] <= 180)  , 'duration']    = 2
+        data.loc[(data['duration'] > 180) & (data['duration'] <= 319)  , 'duration']   = 3
+        data.loc[(data['duration'] > 319) & (data['duration'] <= 644.5), 'duration'] = 4
+        data.loc[data['duration']  > 644.5, 'duration'] = 5
+    
+        return data
+    duration(bank_related);
+    bank_se = bank.loc[: , ['emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']]
+    bank_o = bank.loc[: , ['campaign', 'pdays','previous', 'poutcome']]
+    bank_o['poutcome'].unique()
+    bank_o['poutcome'].replace(['nonexistent', 'failure', 'success'], [1,2,3], inplace  = True)
+    bank_final= pd.concat([bank_client, bank_related, bank_se, bank_o], axis = 1)
+    bank_final = bank_final[['age', 'job', 'marital', 'education', 'default', 'housing', 'loan',
+                         'contact', 'month', 'day_of_week', 'duration', 'emp.var.rate', 'cons.price.idx', 
+                         'cons.conf.idx', 'euribor3m', 'nr.employed', 'campaign', 'pdays', 'previous', 'poutcome']]
+    X_train, X_test, y_train, y_test = train_test_split(bank_final, y, test_size = 0.1942313295, random_state = 101)
+    k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
+    sc_X = StandardScaler()
+    X_train = sc_X.fit_transform(X_train)
+    X_test = sc_X.transform(X_test)
+    logmodel = LogisticRegression() 
+    logmodel.fit(X_train,y_train)
+    logpred = logmodel.predict(X_test)
+    
+    print(round(accuracy_score(y_test, logpred),2)*100)
 #Defineren van plots
 
 def plot_bar_charts():
