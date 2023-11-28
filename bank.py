cc6d1efba9933c567d02d3f7499fdd6311502092
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2023-11-28 23:01:32 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -74,26 +74,65 @@ def tab_one():
     # Toon het voorspelde resultaat
     st.write(f"Voorspelling: {prediction[0]}")
 
-    #Train set
-    # Split the dataset into training and testing sets
-    data = df
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=42)
+    #trainen van data set
+    bank = pd.read_csv('bank.csv', sep = ';')
+    y = pd.get_dummies(bank['y'], columns = ['y'], prefix = ['y'], drop_first = True)
+    bank_client = bank.iloc[: , 0:7]
+        # Label encoder order is alphabetical
+    from sklearn.preprocessing import LabelEncoder
+    labelencoder_X = LabelEncoder()
+    bank_client['job']      = labelencoder_X.fit_transform(bank_client['job']) 
+    bank_client['marital']  = labelencoder_X.fit_transform(bank_client['marital']) 
+    bank_client['education']= labelencoder_X.fit_transform(bank_client['education']) 
+    bank_client['default']  = labelencoder_X.fit_transform(bank_client['default']) 
+    bank_client['housing']  = labelencoder_X.fit_transform(bank_client['housing']) 
+    bank_client['loan']     = labelencoder_X.fit_transform(bank_client['loan']) 
+    #function to creat group of ages, this helps because we have 78 differente values here
+    def age(dataframe):
+        dataframe.loc[dataframe['age'] <= 32, 'age'] = 1
+        dataframe.loc[(dataframe['age'] > 32) & (dataframe['age'] <= 47), 'age'] = 2
+        dataframe.loc[(dataframe['age'] > 47) & (dataframe['age'] <= 70), 'age'] = 3
+        dataframe.loc[(dataframe['age'] > 70) & (dataframe['age'] <= 98), 'age'] = 4
+               
+        return dataframe
     
-    # Create a logistic regression model
-    lr = LogisticRegression()
+    age(bank_client);
+    bank_related = bank.iloc[: , 7:11]
+    def duration(data):
     
-    # Fit the model on the training data
-    lr.fit(X_train, y_train)
+        data.loc[data['duration'] <= 102, 'duration'] = 1
+        data.loc[(data['duration'] > 102) & (data['duration'] <= 180)  , 'duration']    = 2
+        data.loc[(data['duration'] > 180) & (data['duration'] <= 319)  , 'duration']   = 3
+        data.loc[(data['duration'] > 319) & (data['duration'] <= 644.5), 'duration'] = 4
+        data.loc[data['duration']  > 644.5, 'duration'] = 5
     
-    # Make predictions on the testing data
-    y_pred = lr.predict(X_test)
+        return data
+    duration(bank_related);
+    bank_se = bank.loc[: , ['emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']]
+    bank_o = bank.loc[: , ['campaign', 'pdays','previous', 'poutcome']]
+    bank_o['poutcome'].replace(['nonexistent', 'failure', 'success'], [1,2,3], inplace  = True)
+    bank_final= pd.concat([bank_client, bank_related, bank_se, bank_o], axis = 1)
+    bank_final = bank_final[['age', 'job', 'marital', 'education', 'default', 'housing', 'loan',
+                         'contact', 'month', 'day_of_week', 'duration', 'emp.var.rate', 'cons.price.idx', 
+                         'cons.conf.idx', 'euribor3m', 'nr.employed', 'campaign', 'pdays', 'previous', 'poutcome']]
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(bank_final, y, test_size = 0.1942313295, random_state = 101)
     
-    # Calculate the prediction accuracy
-    accuracy = accuracy_score(y_test, y_pred)
+    from sklearn.model_selection import KFold
+    from sklearn.model_selection import cross_val_score
+    from sklearn.metrics import confusion_matrix, accuracy_score
+    k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
+    from sklearn.preprocessing import StandardScaler
+    sc_X = StandardScaler()
+    X_train = sc_X.fit_transform(X_train)
+    X_test = sc_X.transform(X_test)
+    from sklearn.linear_model import LogisticRegression
+    logmodel = LogisticRegression() 
+    logmodel.fit(X_train,y_train)
+    logpred = logmodel.predict(X_test)
     
-    # Print the prediction accuracy
-
-    st.write("Prediction Accuracy:", accuracy)
+    LOGCV = (cross_val_score(logmodel, X_train, y_train, cv=k_fold, n_jobs=1, scoring = 'accuracy').mean())
+    st.write("(round(accuracy_score(y_test, logpred),2)*100)", accuracy)
 
 #Defineren van plots
 
